a9a4cff927a2426c5ff81eee05a6c378d071764f
Tomoki0827223 <146914498+Tomoki0827223@users.noreply.github.com>
2025-02-04 04:14:15 +0900

    確認課題

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,20 +1,34 @@
-#include <iostream>
-#include <thread>
-
-using namespace std;
-
-void PrintThread(uint32_t num) {
-	cout << "thread No" << num << endl;
-}
+﻿#include <iostream>
+#include <string>
+#include <chrono>
 
 int main() {
 
-	thread t1(PrintThread, 1);
-	t1.join();
-	thread t2(PrintThread, 2);
-	t2.join();
-	thread t3(PrintThread, 3);
-	t3.join();
+    // 100,000文字の文字列を初期化
+    std::string long_string(100000, 'a');
 
-	return 0;
+    // コピーにかかる時間を計測
+    auto startCopy = std::chrono::high_resolution_clock::now();
+    std::string copiedString = long_string;
+    auto endCopyTime = std::chrono::high_resolution_clock::now();
+
+    // 移動にかかる時間を計測
+    auto startMove = std::chrono::high_resolution_clock::now();
+    std::string movedString = std::move(long_string);
+    auto endMove = std::chrono::high_resolution_clock::now();
+
+    // 結果をマイクロ秒単位で計算
+    auto copy_duration = std::chrono::duration_cast<std::chrono::microseconds>(endCopyTime - startCopy).count();
+    auto move_duration = std::chrono::duration_cast<std::chrono::microseconds>(endMove - startMove).count();
+
+    // 結果を表示
+    std::cout << "100,000文字を移動とコピーで比較しました。" << std::endl;
+    std::cout << "コピー: " << copy_duration << "µs" << std::endl;
+    std::cout << "移動: " << move_duration << "µs" << std::endl;
+
+    // 続行待ち
+    std::cout << "続行するには何かキーを押してください..." << std::endl;
+    std::cin.get();
+
+    return 0;
 }
