6c06e8e5463fb61baba17671dd6fbbe30591f288
Tomoki0827223 <146914498+Tomoki0827223@users.noreply.github.com>
2025-02-04 01:41:54 +0900

    確認課題

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,53 +1,52 @@
-#include<stdio.h>
-#include<Windows.h>
-#include <time.h>
-#include <functional>
+#include <stdio.h>
 
-//コールバック関数
-void DispResult(int* s, int* kye) {
-	int dice = rand() % 2;
+//親クラス「色」
+class Colors
+{
+public:
+	Colors(const char* a, const char* b) :name(a), mood(b) {}
+	virtual void Mood() {}
+	virtual ~Colors() {} // 仮想デストラクタ
+protected:
+	const char* name;
+	const char* mood;
+};
 
-	if (dice == *kye) {
-		if (dice == 0)
-			printf("%dで丁(偶数)でした。当たり\n", dice);
-		else
-			printf("%dで半(奇数)でした。当たり\n", dice);
+//親の色クラスを継承した「青」クラス
+class Blue : public Colors
+{
+public:
+	Blue() :Colors("Blue", "蒼き深淵の瞳") { printf("This color is %s\n", name); }
+	void Mood() { printf("%s is %s\n", name, mood); }
+};
 
-	}
-	else {
-		if (dice == 1)
-			printf("%dで半(奇数)でした。はずれ\n", dice);
-		else
-			printf("%dで丁(偶数)でした。はずれ\n", dice);
-	}
-}
+//親の色クラスを継承した「赤」クラス
+class Red : public Colors
+{
+public:
+	Red() :Colors("Red", "灼眼のまなざし") { printf("This color is %s\n", name); }
+	void Mood() { printf("%s is %s\n", name, mood); }
+};
 
-void setTimeout(std::function<void(int*, int*)> p, int second, int kye) {
-	//コールバック関数を呼び出す
-	for (int i = 0; i < second; i++) {
-		Sleep(1000);
-		printf("%d...\n", second - i);
-	}
-
-	p(&second, &kye);
-}
+//親の色クラスを継承した「白」クラス
+class White : public Colors
+{
+public:
+	White() : Colors("White", "聖なる白銀") { printf("This color is %s\n", name); }
+	void Mood() { printf("%s is %s\n", name, mood); }
+};
 
 int main() {
-	int kye;
 
-	srand(static_cast<unsigned int>(time(NULL)));
-	printf("丁(偶数)なら0、半(奇数)なら1を打つ\n");
-	scanf_s("%d", &kye);
+	Colors* colors[3] = { new Red(), new Blue(), new White() };
 
-	if (kye == 0) {
-		puts("あなたは丁(偶数)を選びました");
-	}
-	else {
-		puts("あなたは半(奇数)を選びました");
-	}
+	puts("");
 
-	std::function<void(int*, int*)> p = [](int* s, int* kye) { DispResult(s, kye); };
-	setTimeout(p, 3, kye);
+	for (int i = 0; i < 3; i++)
+		colors[i]->Mood();
+
+	for (int i = 0; i < 3; i++)
+		delete colors[i];
 
 	return 0;
 }
